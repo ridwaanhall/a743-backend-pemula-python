129f87bce289faf3565f6769109a3d18c7fe1797
arasopraza <aras.opraza@gmail.com>
2025-01-12 00:30:26 +0700

    refactor: separate settings for dev and prod

--- notes-app-back-end/notes_app_back_end/settings/common.py
+++ notes-app-back-end/notes_app_back_end/settings/common.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gr&*j1(p!)n&fqgf=%)3ql0p_(rr2u^&kzol9^z)l5vsyg(r0+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
