21cc23e768043b3a9610f06cdade5d8c0f03dcfb
arasopraza <aras.opraza@gmail.com>
2025-01-12 00:40:01 +0700

    refactor: add settings for dev and prod

--- notes-app-back-end/notes_app_back_end/settings/common.py
+++ notes-app-back-end/notes_app_back_end/settings/common.py
@@ -19,14 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gr&*j1(p!)n&fqgf=%)3ql0p_(rr2u^&kzol9^z)l5vsyg(r0+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1']
-
 
 # Application definition
 
@@ -70,17 +62,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'notes_app_back_end.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
