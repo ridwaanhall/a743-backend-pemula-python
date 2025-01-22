54bba63aedb189d2946991a5792d5e9b044726dd
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-22 20:54:11 +0700

    refactor: remove unused files and code from notes app

--- notes-app-back-end/notes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('notes/',views.NoteList.as_view(), name='note-list'),
-  path('notes/<uuid:pk>/', views.NoteDetail.as_view(), name='note-detail'),
-]
