0a962fcb74a2bf7a0996afca1c83711e64877e60
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-03-02 14:11:09 +0700

    feat: add shop_app and rest_framework to installed apps

--- OpenShop/settings.py
+++ OpenShop/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'shop_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
